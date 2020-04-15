6cced9dd892e1b80409f635cbf4310909228462d
hellogoose <60927126+hellogoose@users.noreply.github.com>
2020-04-15 14:23:50 +0800

    Create FishScriptReader.py

---
+++ FishScriptReader.py
@@ -0,0 +1,20 @@
+import itertools
+with open('fishes.ocean', 'r') as reader:
+    file = reader.read()
+
+    fish = '<><'
+    fish_end = '><>'
+    wave = '~'
+    current = 0
+
+    result = ''
+    for token, group in itertools.groupby(file.split()):
+        number = len(list(group))
+        if token == fish:
+            current += number
+        elif token == wave:
+            current *= number
+        elif token == fish_end:
+            result += chr(current)
+            current = 0
+    print(result)
